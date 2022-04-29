c4cbcb56435b00d225a6f430751134bdb5c03d10
rujma <rjmalmeida0@gmail.com>
2022-04-29 09:42:06 +0100

    changed files

--- lamport/main.cpp
+++ lamport/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <random>
 
 /* 
     A single processor p starts with an arbitrary value alpha(p). Execution of the system proceeds
@@ -34,12 +35,59 @@ struct Node
     char output_value;
 };
 
-
-
-
-
-
-int main(int argc, char** argv)
+/* Messaging behaviour for the processes */
+/* This class is used to identify who is a general and who is a lieutenant */
+class Properties
 {
+    public:
+        /* */
+        Properties(int source, int m, int n, bool debug = false) : mSource(source), mM(m), mN(n), mDebug(debug)
+        {}
+
+        /* Get the value return by a given process during a messaging round */
+
+        /* Return a default value */
+        char getDefault()
+        {
+            return ONE;
+        }
+
+        /* Identify the fault processes by ID */
+        bool isFaulty(int process)
+        {
+            //return std::any_of(faultyProcessors.begin(), faultyProcessors.end(), [process](int elem)
+            //{
+            //    return process == elem;
+            //});
+        }
+        
+        const int mSource;    /* Process ID for the source process */     
+        const int mM;         /* Number of messaging rounds */      
+        const size_t mN;      /* Number of processes */
+        const bool mDebug;
+};
+
+const int N         = 4;        /* Number of processes */
+const int M         = 1;        /* Number of rounds which is the number of faults to be mitigated (or faulty processors) */
+const int GENERAL   = 3;        /* Processor ID that acts like the general */
+const bool DEBUG    = false;    /* Debug paths */
+
+
+std::vector<int> faultyProcessors;
+
+
+int main()
+{
+    /* Generate processes*/
+    /* Generate which processor ID will be faulty */
+    for (int i = 0; i < M; i++)
+    {
+        faultyProcessors.push_back((rand() % (N - 1)));
+    }
+
+    for (unsigned int i = 0; i < faultyProcessors.size(); i++)
+        std::cout << faultyProcessors.at(i) << std::endl;
+
+
     return 0;
 }
